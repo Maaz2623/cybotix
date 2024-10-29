7ded01854d426d1065013eae055c55f126171466
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-10-29 13:23:30 +0530

    modified:   src/app/(root)/_components/hero.tsx 	modified:   src/app/(root)/layout.tsx

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,7 +1,7 @@
 export default async function RootLayout({children}: {children: React.ReactNode}) {
     return (
-        <>
+        <main>
             {children}
-        </>
+        </main>
     )
 }
